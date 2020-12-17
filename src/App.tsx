24a7d784809ef54657774d90cb2d90e00064c6e2
Roman <roman.simr@eman.cz>
2020-12-17 18:16:57 +0100

    edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoList from "./todo/TodoList";
+import TodoAdd from "./todo/TodoAdd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+import { Navbar, Nav, Container, Row, Col } from "react-bootstrap";
+
+/*
+       <Navbar bg="light" expand="lg">
+         <Navbar.Brand href="#home">TODO</Navbar.Brand>
+         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+         <Navbar.Collapse id="basic-navbar-nav">
+           <Nav className="mr-auto">
+             <Nav.Link href="#home">Domů</Nav.Link>
+             <Nav.Link href="#link">Link</Nav.Link>
+           </Nav>
+         </Navbar.Collapse>
+       </Navbar>;
+*/
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Container>
+          <Row>
+            <Col>
+              <TodoAdd />
+              <TodoList />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
